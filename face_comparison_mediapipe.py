6acc03edf867c87fada9b4f1e110b8dc494a62e0
Mikynek <mikysek.jakub@gmail.com>
2024-04-01 16:21:35 +0200

    feat: comparison between 2 images

--- face_comparison_mediapipe.py
+++ face_comparison_mediapipe.py
@@ -1,12 +1,14 @@
-import cv2
-import numpy as np
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 from file_operations import get_sorted_files, get_image_paths
-    
-def main():
+
+def compare_landmarks(landmarks1, landmarks2):
+    pass
+
+
+def compare_faces_mediapipe(real_data_path, gen_data_path):
     base_options = python.BaseOptions(model_asset_path='helpers/face_landmarker_v2_with_blendshapes.task')
     options = vision.FaceLandmarkerOptions(base_options=base_options,
                                             output_face_blendshapes=True,
@@ -14,20 +16,27 @@ def main():
                                             num_faces=1)
     detector = vision.FaceLandmarker.create_from_options(options)
 
-    data_path = "test02"
-    image_files = get_sorted_files(data_path)
+    real_data_files = get_sorted_files(real_data_path)
+    gen_data_files = get_sorted_files(gen_data_path)
 
-    face_landmarks = []
-    for file_name in image_files:
-        image_path = get_image_paths(data_path, file_name)
-        image = mp.Image.create_from_file(image_path)
+    # Ensure both folders have an equal number of images
+    if len(real_data_files) != len(gen_data_files):
+        raise ValueError("Number of images in 'real-data' and 'gen-data' folders must be the same.")
 
-        detection_result = detector.detect(image)
+    for real_file, gen_file in zip(real_data_files, gen_data_files):
+        real_image_path = get_image_paths(real_data_path, real_file)
+        gen_image_path = get_image_paths(gen_data_path, gen_file)
 
-        print(f"Detected landmarks: {detection_result.face_landmarks}")
-        face_landmarks.append(detection_result.face_landmarks)
-        print(f"Detected face blendshapes: {detection_result.face_blendshapes}")
-        print(f"Detected facial transformation matrixes: {detection_result.facial_transformation_matrixes}")
+        image_real = mp.Image.create_from_file(real_image_path)
+        detection_result_real = detector.detect(image_real)
+
+        image_gen = mp.Image.create_from_file(gen_image_path)
+        detection_result_gen = detector.detect(image_gen)
+
+        print(f"Comparison between {real_file} and {gen_file}")
+        comparison_result = compare_landmarks(detection_result_real.face_landmarks[0], detection_result_gen.face_landmarks[0])
 
 if __name__ == "__main__":
-    main()
+    real_data_path = "real-data"
+    gen_data_path = "gen-data"
+    compare_faces_mediapipe(real_data_path, gen_data_path)
