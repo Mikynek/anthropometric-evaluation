e65a874d24fcd997cc88ec8241ea2da8f99eb84d
Mikynek <mikysek.jakub@gmail.com>
2024-04-05 15:07:42 +0200

    feat: demo code for accessing coordinates and calculating vector distances

--- face_comparison_mediapipe.py
+++ face_comparison_mediapipe.py
@@ -1,3 +1,4 @@
+import math
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
@@ -6,6 +7,13 @@ from helpers.file_operations import get_sorted_files, get_image_paths
 
 def compare_landmarks(landmarks1, landmarks2):
     pass
+
+def calculate_distances(landmark1, landmark2):
+    # Calculate the Euclidean distance between two 3D points
+    distance = math.sqrt((landmark2.x - landmark1.x) ** 2 +
+                         (landmark2.y - landmark1.y) ** 2 +
+                         (landmark2.z - landmark1.z) ** 2)
+    return distance
 
 
 def compare_faces_mediapipe(real_data_path, gen_data_path):
@@ -25,16 +33,30 @@ def compare_faces_mediapipe(real_data_path, gen_data_path):
 
     for real_file, gen_file in zip(real_data_files, gen_data_files):
         real_image_path = get_image_paths(real_data_path, real_file)
-        gen_image_path = get_image_paths(gen_data_path, gen_file)
-
         image_real = mp.Image.create_from_file(real_image_path)
         detection_result_real = detector.detect(image_real)
 
+        gen_image_path = get_image_paths(gen_data_path, gen_file)
         image_gen = mp.Image.create_from_file(gen_image_path)
         detection_result_gen = detector.detect(image_gen)
 
         print(f"Comparison between {real_file} and {gen_file}")
-        comparison_result = compare_landmarks(detection_result_real.face_landmarks[0], detection_result_gen.face_landmarks[0])
+        real_landmarks = detection_result_real.face_landmarks[0]
+        gen_landmarks = detection_result_gen.face_landmarks[0]
+
+        print("Real landmarks sn:", real_landmarks[2].x, real_landmarks[2].y, real_landmarks[2].z)
+        print("Real landmarks n:", real_landmarks[8].x, real_landmarks[8].y, real_landmarks[8].z)
+        print("Gen landmarks sn:", gen_landmarks[2].x, gen_landmarks[2].y, gen_landmarks[2].z)
+        print("Gen landmarks n:", gen_landmarks[8].x, gen_landmarks[8].y, gen_landmarks[8].z)
+
+        real_distance = calculate_distances(real_landmarks[2], real_landmarks[8])
+        gen_distance = calculate_distances(gen_landmarks[2], gen_landmarks[8])
+
+        print(f"Real distance: {real_distance}")
+        print(f"Gen distance: {gen_distance}")
+        print("--------------------------------------------------------------------------------")
+
+        # comparison_result = compare_landmarks(real_landmarks, gen_landmarks)
 
 if __name__ == "__main__":
     real_data_path = "real-data"
